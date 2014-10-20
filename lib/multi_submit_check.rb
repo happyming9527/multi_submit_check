329a1781198e9eb77d88bb68b8049cafc954361a
happyMing <339755551@qq.com>
2014-10-20 15:24:22 +0800

    针对cookie存储的session,添加了用数据库管理键值对的方式.

--- lib/multi_submit_check.rb
+++ lib/multi_submit_check.rb
@@ -1,12 +1,26 @@
 require "multi_submit_check/version"
 
 module MultiSubmitCheck
+  class << self
+    attr_accessor :store_model, :key_column, :value_column, :only_identity_id
+    def configure(&block)
+      yield self
+    end
+  end
 
   #form表单修改
   module FormHelperHack
 
     def token_field
       tk = Digest::SHA1.hexdigest((Time.now.to_i + rand(0xffffff)).to_s)[0..20]
+      if MultiSubmitCheck.store_model.present?
+        MultiSubmitCheck.store_model.where(MultiSubmitCheck.key_column=>request.path,
+                                           MultiSubmitCheck.only_identity_id=>session.id).destroy_all
+        pair = MultiSubmitCheck.store_model.create(MultiSubmitCheck.key_column=> request.path,
+                                                   MultiSubmitCheck.value_column=>tk,
+                                                   MultiSubmitCheck.only_identity_id=> session.id)
+      end
+
       #生成session的名字和当前请求路径一样。
       return hidden_field_tag(:__token__, (session[request.path] = tk))
     end
@@ -30,9 +44,17 @@ module MultiSubmitCheck
     def __check_token__
       return if params[:__token__].blank?
       name=URI.parse(request.referrer).path
-      if session[name] == params[:__token__]
-        session[name] = nil
-        return
+      if  MultiSubmitCheck.store_model.present?
+        MultiSubmitCheckStore.transaction do
+          key_pair = MultiSubmitCheck.store_model.lock(true).where(only_identity_id: session.id, path: name).last
+          key_pair.destroy if key_pair&&key_pair[MultiSubmitCheck.value_column]==params[:__token__]
+          return  if key_pair
+        end
+      else
+        if session[name] == params[:__token__]
+          session[name] = nil
+          return
+        end
       end
       render :status => 404, :text => '您提交的请求已经在处理，请勿重复提交表单!' and return
     end
--- lib/multi_submit_check/version.rb
+++ lib/multi_submit_check/version.rb
@@ -1,3 +1,3 @@
 module MultiSubmitCheck
-  VERSION = "0.0.1"
+  VERSION = "0.1.0"
 end
